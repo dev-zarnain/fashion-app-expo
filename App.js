65d9dccca57d4d932be39464c2b5d13dfffdb3d2
Zarnain <itzshadow46@gmail.com>
2023-11-02 21:52:53 +0500

    fix: export screens

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import "react-native-gesture-handler";
-import { View } from "react-native";
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import Home from "./screens/Home";
-import Dashboard from "./screens/Dashboard";
-import Category from "./screens/Category";
-import Details from "./screens/Details";
+import { Home, Category, Dashboard, Details } from "./screens";
 
 export default function App() {
   return (
---
+++ screens/index.js
@@ -0,0 +1,4 @@
+export { default as Category } from "./Category";
+export { default as Dashboard } from "./Dashboard";
+export { default as Details } from "./Details";
+export { default as Home } from "./Home";
